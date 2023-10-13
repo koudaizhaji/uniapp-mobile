8c0113e30936bb728ca8472f3d98915b7aa26e02
liang_shaowen <liang_shaowen@uxsino.com>
2023-10-13 09:43:01 +0800

    登录优化

---
+++ src/pages/login/register.vue
@@ -0,0 +1,85 @@
+<template>
+    <view class="">
+         <p class="mt-10 mb-2 font-size-6 font-medium text-center">欢迎登录xxxxx</p>
+         <p class="mb-10 font-size-3 text-center color-coolgray">未注册的手机号验证后自动创建账号</p>
+         <view class="flex flex-justify-left grid-items-center h-10 w-80% bg-white m-l-10% border-rd-5">
+             <view class="m-l-2 m-r-2">+86 |</view>
+             <input v-model="phoneNum" type="number" maxlength="11" placeholder="请输入手机号码" class="w-75%"/>
+         </view>
+         <!-- <view class="h-10 w-80% m-l-10% border-rd-5 mt-5">
+            <u--button @click="getCode" :disabled="!validatePhoneNum" color="#F8CC00" shape="circle">获取验证码</u--button>
+         </view> -->
+         <view class="flex flex-justify-left grid-items-center h-10 w-80% bg-white m-l-10% border-rd-5 mt-5">
+             <input v-model="code" type="number" maxlength="6" placeholder="请输入短信验证码" class="w-70% m-l-5"/>
+             <view class="flex flex-justify-center grid-items-center m-l-2 m-r-4 font-size-3 w-25% h-5 bg-rose" :class="{'color-#999 bg-rgb(248, 204, 0)':!validatePhoneNum}" @click="getCode">获取验证码</view>
+             <!-- <view class="w-35%">
+                <button :disabled="!validatePhoneNum" color="#F8CC00"  @click="getCode" >获取验证码</button>
+             </view> -->
+         </view>
+         <view class="flex mt-6 mb-5 m-l-10% font-size-2.5">
+             <view>
+                 <label class="flex flex-justify-left grid-items-center">
+                     <checkbox-group @change="changeChecked">
+                        <label>
+                            <checkbox :checked="isAgree" style="transform:scale(0.6)" />
+                        </label>
+                    </checkbox-group>
+                     <text class="color-#999">
+                         <text>登录即代表同意</text>
+                         <text class="color-#333">[用户协议]</text>
+                         <text>和</text>
+                         <text class="color-#333">[隐私政策]</text>
+                     </text>
+                 </label>
+             </view>
+         </view>
+         <view class="flex">
+             <view class="m-l-10% w-80% flex font-size-3.5 flex-justify-between">
+                 <navigator url="/pages/login/index">密码登录</navigator>
+             </view>
+         </view>
+    </view>
+ </template>
+ 
+ <script>
+     import Validate from '@/utils/validate.js';
+     export default {
+         components: {},
+         computed: {
+			validatePhoneNum() {
+				return Validate.phone(this.phoneNum);
+			}
+		 },
+         data() {
+             return {
+                 phoneNum: '',
+                 code: '',
+                 isAgree: false
+             };
+         },
+         methods: {
+             changeChecked (e) {
+                this.isAgree = e.detail.value.length > 0 ? true : false
+             },
+             getCode() {
+				if(!this.validatePhoneNum) {
+					return;
+				}
+				if (!this.isAgree) {
+					uni.showToast({
+						title: '请先勾选同意协议',
+						icon: 'none',
+					})
+					return;
+				}
+                uni.showToast({
+                    title: '验证码发送成功，请注意查收',
+                    icon: 'none',
+                })
+			}
+         }
+     }
+ </script>
+ <style lang="scss" scoped>
+ </style>
+ 
